8b9426bfc7a03ee03b2f3f2eaf2b1a6426355e5a
AlexVyi <alexandru@epk.ro>
2017-07-17 23:56:02 +0300

    finshed form/added pretty.js and firebase/new modal pictures/aflamaimault+css/

---
+++ js/pretty.js
@@ -0,0 +1,31 @@
+/**
+ * Created by alexandrupetru on 17/07/2017.
+ */
+
+var epk;
+$(document).ready(function () {
+
+epk = firebase.database().ref('/Comenzi')
+
+});
+
+function sendForm() {
+    var myForm = $('#firebaseForm').serializeArray();
+    var resetForm = document.getElementById('firebaseForm')
+    var username = document.getElementById('user-name').value;
+    var email = document.getElementById('user-email').value;
+    var message = document.getElementById('user-message').value;
+
+    if(email && username && message !== ""){
+        epk.push(myForm);
+        resetForm.reset();
+        window.location.href = 'thankyou.html'
+    } else {
+        document.getElementsByName('email')[0].placeholder='Va rugam sa completati toate campurile';
+        document.getElementsByName('name')[0].placeholder='Va rugam sa completati toate campurile';
+        document.getElementsByName('message')[0].placeholder='Va rugam sa completati toate campurile'
+    }
+
+}
+
+
